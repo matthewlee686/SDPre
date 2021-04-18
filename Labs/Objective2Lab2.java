74d19b5cfd21bab35ce82983c5a201dddb234069
Matthew Lee <matthewlee686@gmail.com>
2021-04-18 04:00:08 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Name: Matt L";
+    int age = 25;
+    String state = "State: New York";
+
+    System.out.println(name + " Age: " + age + " " + state);
+  }
+}
