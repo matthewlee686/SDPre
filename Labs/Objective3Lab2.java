b9bb672cc9d1ffc12dc5105c266a2585ee1bc6fc
Matthew Lee <matthewlee686@gmail.com>
2021-04-18 17:34:19 -0400

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 10, side2 = 8, hypotenuse;
+
+    hypotenuse = (side1 * side1) + (side2 * side2);
+
+    hypotenuse = Math.sqrt(hypotenuse);
+
+    System.out.println(hypotenuse);
+  }
+}
