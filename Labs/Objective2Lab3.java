e0d241684850e50553d48a0fd92aa2c8d7bc13c2
Matthew Lee <matthewlee686@gmail.com>
2021-04-18 04:09:05 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 15, num2 = 6, num3 = 2, average;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
