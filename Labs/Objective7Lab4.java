daf18ce91cabc851bb9c6a9f7fcec5c8e665c4b9
Matthew Lee <matthewlee686@gmail.com>
2021-04-26 03:14:19 -0400

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,22 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    //Initialize Variables
+    int sum = 0, counter = 1;
+
+    //While Loop
+    while(counter <= 20) {
+
+      //Calculation
+      sum += counter;
+
+      //Increment
+      counter = counter + 1;
+
+    }
+
+    //Output - will display Sum Total once
+    System.out.println(sum);
+
+  }
+}
