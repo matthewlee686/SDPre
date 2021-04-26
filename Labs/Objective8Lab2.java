121c486474e03eb4f9318b0ca337ae633dd92e93
Matthew Lee <matthewlee686@gmail.com>
2021-04-26 03:35:38 -0400

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,16 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+    //Initialize Total Variable
+    int total = 0;
+
+    //For Loop
+    for(int i = 1; i <= 20; i++) {
+      total += i;
+    }
+
+    //Output
+    System.out.println(total);
+
+  }
+}
